0806460fa4a5ff09049248f0bf420dd21a3533b3
karthick <udhayakarthick.mnm4@gmail.com>
2024-08-10 12:19:27 +0530

    update

--- src/Firebase/Firebase.js
+++ src/Firebase/Firebase.js
@@ -9,14 +9,15 @@ import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// console.log(import.meta.env.VITE_firebaseauthdomain)
 const firebaseConfig = {
-    apiKey: "AIzaSyAi30WJommVBPDIAw8MWDXnJ-A-mqLw2C8",
-    authDomain: "instagramclone-78258.firebaseapp.com",
-    projectId: "instagramclone-78258",
-    storageBucket: "instagramclone-78258.appspot.com",
-    messagingSenderId: "711984621474",
-    appId: "1:711984621474:web:b4f76459c51a276a2734fb",
-    measurementId: "G-HG82CF9X04"
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH,
+    projectId: import.meta.env.VITE_PROJECTID,
+    storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+    messagingSenderId: import.meta.env.VITE_MESSAGINGSENDER,
+    appId: import.meta.env.VITE_APPID,
+    measurementId: import.meta.env.VITE_MEASUREMENTID
 };
 
 
